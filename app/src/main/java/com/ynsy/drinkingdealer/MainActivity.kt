c11ba610bfafbddeef510a3d96cf373898c2ca04
Yena Kim and YeongSeog Jeong <pair+yenarue+jys8773@gmail.com>
2019-06-30 14:38:07 +0900

    feat: 메인화면 추가
    - Kotlin Extension 을 이용한 뷰 바인딩
    
    https://trello.com/c/Fe22HDZC

---
+++ app/src/main/java/com/ynsy/drinkingdealer/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ynsy.drinkingdealer
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main);
+
+        with(this) {
+            button_start.setOnClickListener { showButtonTextOnToast(it) }
+            button_guide.setOnClickListener { showButtonTextOnToast(it) }
+        }
+    }
+
+    fun showButtonTextOnToast(v : View) {
+        Toast.makeText(this, (v as Button).text, Toast.LENGTH_SHORT).show()
+    }
+}
